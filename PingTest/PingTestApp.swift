557e775dfebc0cf76a18e65622b97696f53c2ea5
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-05-05 17:51:15 -0500

    Initial Commit

---
+++ PingTest/PingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingTestApp.swift
+//  PingTest
+//
+//  Created by Chuck Condron on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
