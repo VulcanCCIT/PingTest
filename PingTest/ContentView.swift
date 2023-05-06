ef6823ac05f85d507ac09b4542bd3ee6cbba8a28
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-05-05 20:28:56 -0500

    Initial Commit
    Simple app to ping a host using ICMP

--- PingTest/ContentView.swift
+++ PingTest/ContentView.swift
@@ -3,24 +3,105 @@
 //  PingTest
 //
 //  Created by Chuck Condron on 5/5/23.
+//  Code based on SwiftyPing
+//  https://github.com/samiyr/SwiftyPing.git
 //
+//  Needing to add a textfield or a picker to enter in the ip address... right now it is hard coded
 
 import SwiftUI
 
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  @State var ping: SwiftyPing?
+  @State var response = ""
+  
+  var body: some View {
+    VStack {
+      HStack {
+        Button(action: {
+          ping?.stopPinging()
+          response = ""
+        }, label: {
+          Text("Stop Ping")
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.red)
+            .font(.title2)
+            .fontWeight(.bold)
+            .cornerRadius(20)
+        })
+        
+        Spacer()
+        
+        Button(action: {
+          startPinging()
+        }, label: {
+          Text("Start Ping")
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.green)
+            .font(.title2)
+            .fontWeight(.bold)
+            .cornerRadius(20)
+        })
+        
+      }
+      //scroll indicators are not showing for some reason
+      ScrollView(.vertical) {
+        LazyVStack(spacing: 10) {
+          TextEditor(text: .constant(response))
+            .frame(minHeight: 600)
+            .scrollContentBackground(.hidden)
         }
-        .padding()
+        .background(Color(.black))
+        .foregroundColor(.green)
+        .font(.title3)
+        .fontWeight(.bold)
+      }
     }
+    .padding()
+  }
+  
+  func pingContinuous() {
+    // Ping indefinitely
+    let pinger = try? SwiftyPing(host: "1.1.1.1", configuration: PingConfiguration(interval: 0.5, with: 5), queue: DispatchQueue.global())
+    pinger?.observer = { (response) in
+      let duration = response.duration
+      print(duration)
+    }
+    try? pinger?.startPinging()
+  }
+  
+  func startPinging() {
+    do {
+      ping = try SwiftyPing(host: "1.1.1.1", configuration: PingConfiguration(interval: 1.0, with: 1), queue: DispatchQueue.global())
+      ping?.observer = { (response) in
+        DispatchQueue.main.async {
+          var message = "\(response.duration * 1000) ms"
+          if let error = response.error {
+            if error == .responseTimeout {
+              message = "Timeout \(message)"
+            } else {
+              print(error)
+              message = error.localizedDescription
+            }
+          }
+          self.response.append(contentsOf: "\nPing #\(response.sequenceNumber): \(message)")
+          //self.textView.string.append(contentsOf: "\nPing #\(response.sequenceNumber): \(message)")
+          //self.textView.scrollRangeToVisible(NSRange(location: self.textView.string.count - 1, length: 1))
+        }
+      }
+      //            ping?.targetCount = 1
+      try ping?.startPinging()
+    } catch {
+      response = error.localizedDescription
+    }
+  }
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
